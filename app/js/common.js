8107ea5aa76a91a13121d995019b37551b668364
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2017-02-17 11:59:54 +0300

    responsive screen 900 - ..

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
-$(function() {
+$(window).resize(function(){
 
-	// Custom JS
+});
+
+$(window).ready(function(){
+
+	$(".toggle-menu").click(function(){
+		$(this).toggleClass("active");
+		$(".menu").slideToggle().toggleClass("mobile");
+	});
+
+
+
+	$(".mark").click(function(){
+		$(this).parents(".article-item").addClass("is-delete");
+		$(this).siblings("mark-undo").toggle();
+	});
+	$(".mark-undo").click(function(){
+		$(this).parents(".article-item").removeClass("is-delete");
+	});
 
 });
